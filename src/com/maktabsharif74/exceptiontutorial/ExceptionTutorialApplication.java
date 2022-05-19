c747a09435d6f7e02223ecdb3dbeb81fa167dfe3
maxixking66 <maxixking66@gamil.com>
2022-05-19 09:44:52 +0430

    add example

--- src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
+++ src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
@@ -1,4 +1,22 @@
 package com.maktabsharif74.exceptiontutorial;
 
 public class ExceptionTutorialApplication {
+
+    public static void main(String[] args) {
+        firstMethod();
+    }
+
+    static void firstMethod() {
+        secondMethod();
+        thirdMethod();
+    }
+
+    static void secondMethod() {
+        throw new RuntimeException();
+    }
+
+    static void thirdMethod() {
+        throw new RuntimeException();
+    }
+
 }
