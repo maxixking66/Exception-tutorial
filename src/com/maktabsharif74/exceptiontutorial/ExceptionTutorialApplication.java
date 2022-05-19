0c45af7cfc318b733a3ce97c7e1a89758cce492b
maxixking66 <maxixking66@gamil.com>
2022-05-19 10:42:07 +0430

    add new example

--- src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
+++ src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
@@ -3,19 +3,25 @@ package com.maktabsharif74.exceptiontutorial;
 public class ExceptionTutorialApplication {
 
     public static void main(String[] args) {
-        System.out.println(divide(4, 2));
-        System.out.println(divide(5, 1));
-        System.out.println(divide(6, 0));
+        try {
+//            code
+            System.out.println(divide(5, 0));
+        } catch (BadNumberException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("end!!!");
     }
 
     static int divide(int numberToDivide, int numberToDivideBy) {
         if (numberToDivideBy == 0) {
-            throw new BadNumberException();
+            throw new BadNumberException("badNumber: / by zero - 0");
         }
         return numberToDivide / numberToDivideBy;
     }
 }
 
 class BadNumberException extends RuntimeException {
-
+    public BadNumberException(String message) {
+        super(message);
+    }
 }
