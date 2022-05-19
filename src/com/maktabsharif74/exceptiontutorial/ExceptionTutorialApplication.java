ea3145504fa2ab8c955a58bfe4fd5bb5a2710a3d
maxixking66 <maxixking66@gamil.com>
2022-05-19 14:48:03 +0430

    add new example for multiple catch

--- src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
+++ src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
@@ -1,4 +1,22 @@
 package com.maktabsharif74.exceptiontutorial;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class ExceptionTutorialApplication {
+    public static void main(String[] args) {
+
+        try {
+            a();
+        } catch (SQLException | IOException | InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("exception");
+        }
+
+    }
+
+    static void a() throws SQLException, IOException, InterruptedException {
+
+    }
+
 }
