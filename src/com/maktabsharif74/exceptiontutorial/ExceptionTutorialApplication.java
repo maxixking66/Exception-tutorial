3d00c07ef6f241471aad4b05e5681732e19e8a11
maxixking66 <maxixking66@gamil.com>
2022-05-19 15:49:11 +0430

    add child exceptions

--- src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
+++ src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
@@ -1,22 +1,40 @@
 package com.maktabsharif74.exceptiontutorial;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 public class ExceptionTutorialApplication {
     public static void main(String[] args) {
 
         try {
             a();
-        } catch (SQLException | IOException | InterruptedException e) {
-            e.printStackTrace();
-            System.out.println("exception");
+        } catch (ChildException e) {
+            System.out.println("child");
+        } catch (BrotherException e) {
+            System.out.println("brother");
+        } catch (ParentException e) {
+            System.out.println("parent");
         }
 
     }
 
-    static void a() throws SQLException, IOException, InterruptedException {
-
+    static void a() throws ParentException,
+            ChildException, GrandSonException {
+        throw new BrotherException();
     }
+
+}
+
+class ParentException extends Exception {
+
+}
+
+
+class ChildException extends ParentException {
+
+}
+
+class BrotherException extends ParentException {
+
+}
+
+class GrandSonException extends ChildException {
 
 }
