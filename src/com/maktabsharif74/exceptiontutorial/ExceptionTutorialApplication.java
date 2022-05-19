f6a6bc65fac20d31f82520ec40cc5e80006d6ea0
maxixking66 <maxixking66@gamil.com>
2022-05-19 10:06:33 +0430

    add throw exmaple

--- src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
+++ src/com/maktabsharif74/exceptiontutorial/ExceptionTutorialApplication.java
@@ -1,4 +1,21 @@
 package com.maktabsharif74.exceptiontutorial;
 
 public class ExceptionTutorialApplication {
+
+    public static void main(String[] args) {
+        System.out.println(divide(4, 2));
+        System.out.println(divide(5, 1));
+        System.out.println(divide(6, 0));
+    }
+
+    static int divide(int numberToDivide, int numberToDivideBy) {
+        if (numberToDivideBy == 0) {
+            throw new BadNumberException();
+        }
+        return numberToDivide / numberToDivideBy;
+    }
+}
+
+class BadNumberException extends RuntimeException {
+
 }
